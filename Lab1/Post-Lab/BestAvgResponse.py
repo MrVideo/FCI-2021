9877ea101939ac644d5e4527b5a2c6fd6009f182
Mario Merlo <m20_11@icloud.com>
2021-03-09 17:18:56 +0100

    New folder structure

---
+++ Lab1/Post-Lab/BestAvgResponse.py
@@ -0,0 +1,16 @@
+import requests
+
+sites = ('http://www.google.com', 'http://www.youtube.com', 'http://www.polimi.it', 'http://www.wikipedia.org', 'http://www.amazon.com', 'http://www.twitter.com')
+times = []
+avg = []
+
+for url in sites:
+    for req in range(10):
+        r = requests.get(url)
+        times.append(r.elapsed.microseconds/1000)
+
+    avg.append(sum(times)/len(times))
+
+best = min(avg)
+ind = avg.index(min(avg))
+print('Il sito con tempo di risposta migliore è: ', sites[ind])
