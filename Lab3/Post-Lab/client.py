c1353b0a66ddbb5b1b4ef828d07095c2b7ca07dc
Mario Merlo <m20_11@icloud.com>
2021-04-14 15:23:02 +0200

    Finished post-lab activities

---
+++ Lab3/Post-Lab/client.py
@@ -0,0 +1,37 @@
+from socket import *
+
+serverName = 'localhost'
+serverPort = 12000
+serverAdd = (serverName, serverPort)
+
+try:
+    cSocket = socket(AF_INET, SOCK_STREAM)
+    cSocket.connect(serverAdd)
+
+    while True:
+        m = input('Inserisci una stringa: ')
+        strlen = len(m)
+        start = 0
+        end = start + 100
+
+        if m == '.':
+            cSocket.send(m.encode('utf-8'))
+            cSocket.close()
+            break
+        elif strlen <= 100:
+            cSocket.send(m.encode('utf-8'))
+        else:
+            while strlen > 0:
+                mSplit = m[start:end]
+                start = end
+                end += 100
+                cSocket.send(mSplit.encode('utf-8'))
+                print('Segmento inviato: ', mSplit)
+                strlen -= 100
+
+except ConnectionRefusedError:
+    print('Il server ha rifiutato la connessione')
+except BrokenPipeError:
+    print('La connessione si è interrotta per motivi sconosciuti')
+finally:
+    cSocket.close()
---
+++ Lab3/Post-Lab/server.py
@@ -0,0 +1,26 @@
+from socket import *
+
+serverPort = 12000
+serverAdd = ('', serverPort)
+
+sSocket = socket(AF_INET, SOCK_STREAM)
+sSocket.bind(serverAdd)
+sSocket.listen(1)
+
+print('Server ready')
+
+while True:
+    conSocket, cAdd = sSocket.accept()
+    print('Connection to ', cAdd, ' established')
+    num = 0
+
+    while True:
+        m = conSocket.recv(2048)
+        m = m.decode('utf-8')
+        if m == '.':
+            conSocket.close()
+            break
+        else:
+            strlen = len(m)
+            print('Segment ', str(num), ': ', strlen, ' characters')
+            num += 1
